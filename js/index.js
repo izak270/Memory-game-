230b734c0cd74b4d3f520f3a3e68bd83f582fa2c
izak270 <izak270@gmail.com>
2019-12-27 21:03:45 +0200

    sorting cards function

--- js/index.js
+++ js/index.js
@@ -8,20 +8,47 @@ for (let i = 0; i < 3; i++) {
     container.appendChild(row)
     for (let i = 0; i < 4; i++) {
         const card = document.createElement("div")
-        card.classList.add("card", "col-3", "col-md-3", "col-xl-3", "back")
+        const img = document.createElement("img")
+        img.src = "./images/back.jpg"
+        img.id.add =
+            card.classList.add("card", "col-3", "col-md-3", "col-xl-3")
+        card.appendChild(img)
         row.appendChild(card)
     }
 }
+const cardsList = ["king1.jpg", 'quin3.jpg', 'ase.png', '10.png', '9.png', 'jack.jpg']
+let i = 0
 
-const flipCard = (e)=>{
-    e.target.classList.remove("back")
+const flipCard = (e) => {
+    if(e.target.src == "./images/king1.jpg"){
+        return
+    }
+    else{
+    e.target.src = `./images/${cardsList[selected[i+1]]}`
+    i++
+    return i
+    }
+
 }
+let selected = [0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5]
+const choosNumber = () => {
+    var j, x, i;
+    for (i = selected.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = selected[i];
+        selected[i] = selected[j];
+        selected[j] = x;
+
+    }
+    return selected
+}
+choosNumber()
 const target = document.querySelectorAll(".card")
 
 const cards = [...target];
 console.log(cards.length);
 cards.forEach(card => {
-    card.addEventListener("click",flipCard)
+    card.addEventListener("click", flipCard)
 })
 
 
