f391ac5683bc035516af5419f1f880f284ddeeb2
izak270 <izak270@gmail.com>
2019-12-27 12:51:22 +0200

    refactor to dynamic

--- js/index.js
+++ js/index.js
@@ -1,19 +1,27 @@
-class MemoryGame{
-    constructor(){
-        this.cards = new Cards
-        this.initEvents()
-        
-    }
-    
-}
-class Cards{
-    constructor(){
-       this.allCards =[]
-       this.allCards.push($(".row"));
-        for (let i = 0; i < 3; i++) {
-            this.allCards[i] = $(`.row:nth-child(${i + 1}) .cell`);
+const container = document.createElement("div")
+
+container.classList.add("container")
+document.body.appendChild(container)
+for (let i = 0; i < 3; i++) {
+    const row = document.createElement("div")
+    row.classList.add("row")
+    container.appendChild(row)
+    for (let i = 0; i < 4; i++) {
+        const card = document.createElement("div")
+        card.classList.add("card", "col-3", "col-md-3", "col-xl-3", "back")
+        row.appendChild(card)
     }
 }
-    
+
+const flipCard = (e)=>{
+    e.target.classList.remove("back")
 }
+const target = document.querySelectorAll(".card")
+
+const cards = [...target];
+console.log(cards.length);
+cards.forEach(card => {
+    card.addEventListener("click",flipCard)
+})
+
 
